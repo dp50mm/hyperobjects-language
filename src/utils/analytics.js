d646e364d5140105113649ca78ff9fa7b70c1b16
Erwin Hoogerwoord <erwinhoogerwoord@erwins-mbp-2.home>
2020-11-07 13:32:19 +0100

    removed some unnecessary dependencies

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,12 +1,12 @@
-import ReactGA from 'react-ga'
+// import ReactGA from 'react-ga'
 var analytics = {
   count: 0,
   shouldLogEvents: false, //
   initialized: false,
   initializeGoogleAnalytics: function() {
-    ReactGA.initialize('UA-92339423-4')
-    this.shouldLogEvents = true
-    this.initialized = true
+    // ReactGA.initialize('UA-92339423-4')
+    // this.shouldLogEvents = true
+    // this.initialized = true
   },
   initialize: function() {
     if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
@@ -17,11 +17,11 @@ var analytics = {
   },
   event: function(action, category, label) {
     if(this.initialized && this.shouldLogEvents) {
-      ReactGA.event({
-        category: category,
-        action: action,
-        label: label
-      });
+      // ReactGA.event({
+      //   category: category,
+      //   action: action,
+      //   label: label
+      // });
     }
   }
 }
